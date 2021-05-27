c3c63bd889cfddcccdec9e53717cf44b7a7a6cb0
Jerry <iiiplay001@gmail.com>
2021-05-27 18:31:20 +0800

    update static file link

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    sub_title = models.CharField(max_length=80)
+    create_date = models.DateField(auto_now_add=True)
+    information = models.TextField(blank=True)
+    image_url = models.CharField(max_length=255,blank=True)
+    url = models.CharField(max_length=255,blank=True)
